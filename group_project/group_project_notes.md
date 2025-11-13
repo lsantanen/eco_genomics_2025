abc1fd46503e3c689c354433ffe7e9ed73156f66
Lauren Santanen <lsantane@node253.cluster>
2025-11-13 16:56:01 -0500

    11/13 project work (ANGSD)

--- group_project/group_project_notes.md
+++ group_project/group_project_notes.md
@@ -17,3 +17,17 @@
 -   work on agsd file
 
 -   next time check with Steve about some things for the `ANGSD.sh` file saved in `group_project/myscripts`
+
+### 11/13/2025: Second open lab day
+
+-   copy `diversity_wrapper.sh` from `population_genomics/myscripts` into `group_project/myscripts`
+
+-   change `ANGSD.sh` to create a directory in `/gpfs1/cl/ecogen/pbio6800/GroupProjects/spruceAgogo/RDA` (called ANGSD) so other people can access it
+
+-   run `ANGSD.sh` for all pops so output file has all the data needed to put into RDA
+
+    -   Changed suffix to `Poly` rather than `ALL`
+
+    -   `ANGSD.sh` and `ANGSD_pops.sh` are both saved under `~/projects/eco_genomics_2025/group_project/myscripts`
+
+    -   Gave `sbatch` a run time of 48 hours just to ensure it would have enough time
--- group_project/myscripts/ANGSD.sh
+++ group_project/myscripts/ANGSD.sh
@@ -8,7 +8,7 @@ module load gcc angsd
 
 ### Set up directories and variables
 
-#mkdir ~/projects/eco_genomics_2025/population_genomics/mydata/ANGSD
+mkdir /gpfs1/cl/ecogen/pbio6800/GroupProjects/spruceAgogo/RDA/ANGSD
 
 INPUT="/gpfs1/cl/ecogen/pbio6800/PopulationGenomics/bams"
 
@@ -16,25 +16,36 @@ OUT="/gpfs1/cl/ecogen/pbio6800/GroupProjects/spruceAgogo/RDA"
 
 REF="/gpfs1/cl/ecogen/pbio6800/PopulationGenomics/ref_genome/Pmariana/Pmariana1.0-genome_reduced.fa"
 
-MYPOP="2103" #see if this will need to be run independently for each pop or if I can make a list
+#MYPOP="2019"
+#MYPOP="2020" 
+#MYPOP="2021" 
+#MYPOP="2022" 
+#MYPOP="2024" 
+#MYPOP="2027" 
+#MYPOP="2030" 
+#MYPOP="2032" 
+#MYPOP="2100" 
+#MYPOP="2101" 
+#MYPOP="2103" 
+#MYPOP="2505" 
 
-ls ${INPUT}/${MYPOP}*sorted.rmdup.bam >${OUT}/${MYPOP}_bam.list
+ls ${INPUT}/2*sorted.rmdup.bam >${OUT}/ANGSD_for_RDA_bam.list
 
 
 
 # File suffix to distinguish analysis choices, like "All" or "Poly" depending on whether you're analyzing all the sites or just the polymorphic ones
 
-SUFFIX="ALL"
+SUFFIX="Poly"
 
 
 ###########################################
 #  Estimating Genotype Likelihoods (GLs)  #
 ###########################################
 
-angsd -b ${OUT}/${MYPOP}_bam.list \
+angsd -b ${OUT}/ANGSD_for_RDA_bam.list \
 -ref ${REF} \
 -anc ${REF} \
--out ${OUT}/${MYPOP}_${SUFFIX} \
+-out ${OUT}/ANGSD_for_RDA_${SUFFIX} \
 -nThreads 10 \
 -remove_bads 1 \
 -C 50 \
